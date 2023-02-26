6ecf8b3d696ed2bcf183d776c29d7a684ba3da74
Mozart Maia <mozartmaia89@gmail.com>
2023-02-26 15:20:40 -0300

    generating mermaid from textfield

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Mermaid from "./Mermaid";
+// import example from "./example";
+// import mermaidAPI from "mermaid/dist/mermaidAPI";
+import mermaid from "mermaid";
+// import Mermaid from "react-mermaid2";
+import { useState, useEffect } from "react";
+// import { __esModule } from "react-mermaid";
+// import mermaidAPI from "mermaid/dist/mermaidAPI";
+
+// mermaid.initialize({
+//   startOnLoad: true,
+//   theme: "default",
+//   securityLevel: "loose",
+// });
 
 function App() {
+  const [text, setText] = useState();
+  const [preview, setPreview] = useState();
+  const [output, setOutput] = useState("");
+
+  useEffect(() => {
+    // mermaid.contentLoaded();
+    // handleParse();
+
+    const resultRender = mermaid.render("graph", preview).then((res) => {
+      const { svg } = res;
+      setOutput(svg);
+      // console.log(svg);
+    });
+    // console.log("resultrender:", resultRender);
+  }, [preview]);
+
+  const handleClick = () => {
+    setPreview(text);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>React Mermaid</h1>
+      <div>
+        <textarea
+          name=""
+          id="input"
+          cols="41"
+          rows="15"
+          onChange={(e) => setText(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {text}
+        </textarea>
+      </div>
+      <div>
+        Your code (for debugging):{" "}
+        <pre className="mermaid" id="preview">
+          {preview}
+        </pre>
+      </div>
+      <div>
+        <button type="button" id="render" onClick={handleClick}>
+          Render
+        </button>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: output }} />
     </div>
   );
 }
